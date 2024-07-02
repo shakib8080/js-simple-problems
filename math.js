ced6eca7bfc4327f3f052ebde92f67c6e67784b4
shakib8080 <mdshakibulhasan0808@gmail.com>
2024-07-02 12:46:14 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,53 @@
+// Math.abs()
+
+// let number= -50 ;
+// let output= Math.abs(number);
+// console.log(output);
+
+// Math.ceil()
+
+// let number = 5.84895;
+// let result = Math.ceil(number);
+// console.log(result);
+
+// Math.floor()
+
+// let number = 9.94949; 
+// let output = Math.floor(number);
+// console.log(output);
+
+// Math.round ()
+
+// let number = 23.6746;
+// let result = Math.round(number);
+// console.log(result);
+// ||
+// let number = 28.134;
+// let result = Math.round(number);
+// console.log(result);
+
+// Math.random()
+
+// let output = Math.random();
+// console.log(output);
+
+// ||
+// let output = Math.random()*10;
+// console.log(output);
+
+// ||
+// let output = Math.random()*6;
+// let floor = Math.floor(output);
+// console.log(floor);
+// ||
+// let output = Math.random()*6;
+// let round = Math.round(output);
+// console.log(round);
+
+// for loop 
+//  for(let i =0;i<=30;i++){
+//     let output = Math.random()*6;
+//     let round = Math.round(output);
+//     console.log(round);
+//  }
+ 
